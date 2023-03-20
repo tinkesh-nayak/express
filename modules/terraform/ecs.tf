af3ceb7a449db2c8f50753c51f0d45f9cb957d3c
tinkesh-nayak <97432390+tinkesh-nayak@users.noreply.github.com>
2023-03-20 08:27:53 +0530

    Update ecs.tf

--- modules/terraform/ecs.tf
+++ modules/terraform/ecs.tf
@@ -11,13 +11,13 @@ data "aws_iam_policy_document" "ecs_tasks_execution_role" {
 }
 
 resource "aws_iam_role" "ecs_tasks_execution_role" {
-  name               = "ecsTaskRole"
+  name               = "ecsTaskExecutionRole"
   assume_role_policy = "${data.aws_iam_policy_document.ecs_tasks_execution_role.json}"
 }
 
 resource "aws_iam_role_policy_attachment" "ecs_tasks_execution_role" {
   role       = "${aws_iam_role.ecs_tasks_execution_role.name}"
-  policy_arn = "arn:aws:iam::aws:policy/AmazonECS_FullAccess"
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceforEC2Role"
 }
 
 data "aws_ecs_task_definition" "nodeapp" {
