68b78a647187f48b12cb6b16d1d8f3ba4a8b6bbc
tinkesh-nayak <97432390+tinkesh-nayak@users.noreply.github.com>
2023-03-20 07:34:30 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,9 @@
+provider "aws" {
+  access_key = "AKIA5FMVIZDRVLC6AVFP"
+  secret_key = "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"
+  region = var.aws_region
+}
+
 module "terraform" {
   source = "modules/terraform"
 }
